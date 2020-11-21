d1a6d8548edd8d37f0b92ca60fba756afdaa8c6c
zzt96 <136901341@qq.com>
2020-11-21 16:14:47 -0500

    add event

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,9 @@
 <script>
     import FullCalendar from '@fullcalendar/vue'
     import dayGridPlugin from '@fullcalendar/daygrid'
+    import timeGridPlugin from '@fullcalendar/timegrid'
     import interactionPlugin from '@fullcalendar/interaction'
+    import {INITIAL_EVENTS, createEventId} from '../JS/event-utils'
 
     export default {
         name: 'appHome',
@@ -11,23 +13,59 @@
         data() {
             return {
                 calendarOptions: {
-                    plugins: [ dayGridPlugin, interactionPlugin ],
+                    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    },
                     initialView: 'dayGridMonth',
+                    initialEvents: INITIAL_EVENTS,
+                    editable: true,
+                    selectable: true,
+                    selectMirror: true,
+                    dayMaxEvents: true,
+                    weekends: true,
+                    eventClick: this.handleEventClick,
+                    eventsSet: this.handleEvents,
                     dateClick: this.handleDateClick,
-                    events: [
-                        { title: 'event 1', date: '2019-04-01' },
-                        { title: 'event 2', date: '2019-04-02' }
-                    ]
-                }
+                },
+                currentEvents: []
             }
         },
         methods: {
-            handleDateClick: function(arg) {
-                alert('date click! ' + arg.dateStr)
+            handleDateClick: function (selectInfo) {
+                let title = prompt('please enter a new event')
+                let calendarApi = selectInfo.view.calendar
+
+                calendarApi.unselect() // clear date selection
+
+                if (title) {
+                    calendarApi.addEvent({
+                        id: createEventId(),
+                        title: title,
+                        start: selectInfo.dateStr,
+                        end: selectInfo.endStr,
+                        allDay: selectInfo.allDay
+                    })
+                }
+            },
+            handleEventClick(clickInfo) {
+                if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+                    clickInfo.event.remove()
+                }
+            },
+            handleEvents(events) {
+                this.currentEvents = events
             }
         }
     }
 </script>
 <template>
-    <FullCalendar :options="calendarOptions" />
+    <FullCalendar :options="calendarOptions">
+        <template v-slot:eventContent='arg'>
+            <b>{{ arg.timeText}} </b>
+            <i>{{ arg.event.title}}</i>
+        </template>
+    </FullCalendar>
 </template>
