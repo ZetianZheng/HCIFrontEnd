03ba134af3081b2aa92b29c3d5810f944fd4fdc6
zzt96 <136901341@qq.com>
2020-11-22 21:23:34 -0500

    focus page styles

--- src/JS/event-utils.js
+++ src/JS/event-utils.js
@@ -21,3 +21,20 @@ export function createEventId() {
 export function todayStr(){
     return new Date().toISOString().replace(/T.*$/, '')
 }
+
+export const TEST_EVENT_DATA = [
+    {id:1, title:'events 1', content:'event content1',
+        start_time:'18:00:00', end_time:'20:00:00'},
+    {id:2, title:'events 2', content:'event content2',
+        start_time:'18:00:00', end_time:'20:00:00',
+        subevent:[
+            {id:1, title:'subevents 2', content:'subevent content1'},
+            {id:2, title:'subevents 2', content:'subevent content2'},
+            {id:3, title:'subevents 2', content:'subevent content3'},
+        ]
+    },
+    {id:3, title:'events 3', content:'event content3',
+        start_time:'18:00:00', end_time:'20:00:00'},
+    {id:4, title:'events 4', content:'event content4',
+        start_time:'18:00:00', end_time:'20:00:00'},
+]
--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
 import {router} from './router/index'
+import App from './App.vue'
+import store from './store/index'
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue'
+
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css';
+import 'swiper/css/swiper.css'
+
+Vue.config.productionTip = false
 
 Vue.use(VueAwesomeSwiper);
 
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
-
-
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
